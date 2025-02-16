5de0e9cd07e1dbfd69af3cb039356f166ac6daac
kuku <154352363+kukulemma@users.noreply.github.com>
2025-02-16 02:39:00 -0500

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   
   return (
     <>
       <div className="App">
-        <h1>hello</h1>
+       <Home/>
       </div>
       
     </>
